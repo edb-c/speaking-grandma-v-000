e386971ca6e0c81f33c006cb2ef3e9b92aa68247
Ethel C <cofresi.ethel@gmail.com>
2018-03-21 02:55:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,8 @@ def speak_to_grandma(phrase)
      puts 'HUH?! SPEAK UP, SONNY!'
    elseif phrase.upcase
      puts 'NO, NOT SINCE 1938!'
-   elseif 
+   end
+   if 
      phrase = 'I LOVE YOU GRANDMA!'
      puts 'I LOVE YOU TOO PUMPKIN!'
    end
